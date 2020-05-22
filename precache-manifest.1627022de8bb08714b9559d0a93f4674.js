e97a60b351c05608889a2b9db774862ef990e65b
pksuarez <pablokalet@gmail.com>
2020-05-22 03:44:32 -0600

    Updates

---
+++ precache-manifest.1627022de8bb08714b9559d0a93f4674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49e93cfa9fcbdaa069ebb368a89fa76",
+    "url": "/papelitos/index.html"
+  },
+  {
+    "revision": "8f33ce9593ac02c21ce2",
+    "url": "/papelitos/static/js/2.53af2f9b.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/papelitos/static/js/2.53af2f9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7d2cf445c8ef8101b6",
+    "url": "/papelitos/static/js/main.7b2fb0ee.chunk.js"
+  },
+  {
+    "revision": "7e891d097aff50d001ae",
+    "url": "/papelitos/static/js/runtime-main.7632c966.js"
+  }
+]);
